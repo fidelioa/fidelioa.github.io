4b0a20353d4a7eb3fd4fc4e81bcf74e977bdbcc7
Ankit Shah <ankitshahas008@gmail.com>
2026-05-30 19:25:51 +0545

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBq2uEulFacFnzk86agcRhuQD1UNlZ_UTE",
+    authDomain: "the-grand-budapest.firebaseapp.com",
+    projectId: "the-grand-budapest",
+    storageBucket: "the-grand-budapest.firebasestorage.app",
+    messagingSenderId: "656352616063",
+    appId: "1:656352616063:web:4f807b5f16b2cb059d694a",
+    measurementId: "G-87X1RHHV3F"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
